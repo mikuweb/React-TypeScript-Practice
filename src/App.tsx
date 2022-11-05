00220721bfd140ed84575f9fa26180edbe024c06
Miku Kawata <100392926+mikuweb@users.noreply.github.com>
2022-11-05 14:49:37 -0700

    Fixed error Fixed error by defining the type of retrieved data

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,30 @@ import { useState } from "react";
 import { Todo } from "./Todo";
 import "./styles.css";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+        console.log(res);
+      });
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>Get Data</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userid} />
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </div>
   );
